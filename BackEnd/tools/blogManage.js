1997aa3a4666918d80488a19619b4f86c8f045c5
wangpengyuan01 <wangpengyuan01@appledeMacBook-Pro.local>
2020-11-11 11:06:06 +0800

    add function to db.js

--- BackEnd/tools/blogManage.js
+++ BackEnd/tools/blogManage.js
@@ -1,7 +1,19 @@
 'use strict';
 
-const { mongoose, blogModel, blogInsert } = require("../utils/db");
+const db = require("../utils/db");
 const { parseMetaBody } = require("../utils/parseMarkdown");
 
 let content = parseMetaBody("../source/_posts/hello-world.md");
-blogInsert(blogModel, content);
+// db.blogInsert(db.blogModel, content);
+// db.blogDelete(db.blogModel, "Hello World");
+// db.blogUpdate(db.blogModel, "Hello World");
+// db.addViewCount(db.blogModel, "Hello World");
+// db.addLike(db.blogModel, "Hello World1");
+// db.addDislike(db.blogModel, "Hello World");
+// db.findByTitle(db.blogModel, "Hello World").then((data) => {
+//     console.log(data);
+// });
+// db.blogUpdate(db.blogModel, "Hello World", {"updateDate": Date.now(), tag: ["Hello"]});
+// db.addComment(db.blogModel, "Hello World", 
+//     {"body": "good article", "date": new Date().toLocaleDateString(), "email": "1174555847@qq.com", "name": "wpy"});
+// db.addCommentVote(db.blogModel, "Hello World", new Date("2020-11-10 16:00:00.000Z"));
